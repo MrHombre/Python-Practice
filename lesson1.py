931ba6ff2ed66131667c56ad6199e1b94d53b525
James Lett <jlett89@gmail.com>
2017-01-22 22:54:13 -0800

    lesson 1

---
+++ lesson1.py
@@ -0,0 +1,33 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 3 + 4
+7
+>>> 3 * 20
+60
+>>> 12 /4
+3.0
+>>> 8 + 2 * 10
+28
+>>> (8 + 2) * 10
+100
+>>> 18 / 4
+4.5
+>>> 18//4
+4
+>>> 18%4
+2
+>>> 5 * 5* 5
+125
+>>> 5 ** 3
+125
+>>> 8 ** 2
+64
+>>> tuna = 5
+>>> 20 + tuna
+25
+>>> bacon = 18
+>>> bacon / tuna
+3.6
+>>> bacon//tuna
+3
+>>> 

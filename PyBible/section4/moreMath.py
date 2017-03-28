0939c8ef74f4c5189f1741a0d7bda8de81012625
James Lett <jlett89@gmail.com>
2017-03-28 12:52:42 -0700

    more math

---
+++ PyBible/section4/moreMath.py
@@ -0,0 +1,26 @@
+import math
+
+round(2.3)
+round(2.1)
+
+# Things you can do with Math Module
+math.floor(1.5)
+math.ceil(2.1)
+math.pi
+math.e
+math.sin(math.pi / 2)
+math.sin(math.pi)
+math.floor(math.sin(math.pi))
+math.cos(0)
+math.asin(0)
+math.acos(0)
+math.hypot(3,4)
+#Power of
+math.pow(2,3)
+math.pow(9,2)
+#exp
+math.exp(2)
+#natural log
+math.log(math.e)
+math.log10(1000)
+math.log2(8)

12dde29fe3d9d0e2b57c63fddf0a8df68bf8958d
James Lett <jlett89@gmail.com>
2017-03-27 18:13:43 -0700

    Just starting again
    Nothing to really see here

---
+++ PyBible/varibles_script.py
@@ -0,0 +1,9 @@
+#just doing some basic stuff nothing to see here.
+first_number = 1 + 1 + 10
+print(first_number)
+
+second_number = 123+28
+print(second_number)
+
+total = first_number + second_number
+print(total)
